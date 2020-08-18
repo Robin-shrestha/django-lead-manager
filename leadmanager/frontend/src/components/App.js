10a9afd05824781e2d57df7c47817e11b7ff5fa5
Robin-shrestha <rsresthas@gmail.com>
2020-08-18 11:54:17 +0545

    first commit

---
+++ leadmanager/frontend/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+// class App extends Component {
+//   render() {
+//     return <h1>hellps</h1>;
+//   }
+// }
+const App = () => {
+  return <div>pythonasdasdasdas</div>;
+};
+
+export default App;
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ leadmanager/leads/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path 
+from rest_framework import routers
+from .api import LeadViewSet
+
+router = routers.DefaultRouter()
+router.register('api/leads', LeadViewSet, basename='leads')
+
+
+urlpatterns = router.urls
