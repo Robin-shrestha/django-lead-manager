31ba4d72a3dd5c562e766232689875c3819fb0a4
Robin-shrestha <rsresthas@gmail.com>
2020-08-20 11:16:33 +0545

    added an .env file

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -20,8 +22,8 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4#s8&(w3r-+qrur#rt)jc_(22b3-^0so#=2d0_j7hj=+ilo_r3'
-
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
